ade21bee67187afb1a7cd285607e02ec78b70110
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2021-03-05 16:09:56 -0600

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAj52d5GYu-kJlbRLph1DRmvN8gIOMTIv8",
+    authDomain: "teams-clone-bf3d9.firebaseapp.com",
+    projectId: "teams-clone-bf3d9",
+    storageBucket: "teams-clone-bf3d9.appspot.com",
+    messagingSenderId: "937767618216",
+    appId: "1:937767618216:web:d8c7cc170779045a5c649d",
+    measurementId: "G-3WEJLZWV87"
+  };
