efcd6388e5e398a0c995646b158735df8abe9f25
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2021-03-07 11:22:01 -0600

    material ui install and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-     <h1>Let's build Teams</h1>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Header />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
